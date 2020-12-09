5be215455ee421af090f32d825b61ad6822ec85c
Peiyi Guan <peiyi.dev@gmail.com>
2020-12-08 20:35:56 -0500

    adding instoremed urls

--- config.js
+++ config.js
@@ -5,6 +5,10 @@ module.exports = {
         `https://appointment.southvanmed.com/ping`,
         `https://frontdesk.southvanmed.com/ping`,
         `https://admin.southvanmed.com/ping`,
-        `https://api.southvanmed.com/schedule/`
+        `https://api.southvanmed.com/schedule/`,
+        `https://appointment.instoremed.com/ping`,
+        `https://frontdesk.instoremed.com/ping`,
+        `https://admin.instoremed.com/ping`,
+        `https://api.instoremed.com/schedule/`
     ]
 }
